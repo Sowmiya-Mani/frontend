6d4b84c8bbf614120647a1e598acc9156bbdb88b
amarell <mujagicamar@gmail.com>
2022-04-09 14:23:40 +0200

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Login from "./routes/Login";
+import Register from "./routes/Register";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" exact element={<Home />} />
+      <Route path="/login" exact element={<Login />} />
+      <Route path="/register" exact element={<Register />} />
+    </Routes>
+  );
+}
+
+export default App;
