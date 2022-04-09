296c41b6b0d097df3ae96e5661752c366b7f428b
amarell <mujagicamar@gmail.com>
2022-04-09 19:19:51 +0200

    Added decorative wave to the homepage

--- src/routes/Home/MiddleSection/MiddleSection.jsx
+++ src/routes/Home/MiddleSection/MiddleSection.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+import { ReactComponent as Waves } from "./../../../images/wave.svg";
 import styles from "./MiddleSection.module.scss";
 
 function MiddleSection() {
   return (
     <div className={styles.wrapper}>
+      <Waves className={styles.waves} />
       <h1 className={styles.header}>How does it work?</h1>
       <p className={styles.paragraph}>
         Make an account by clicking signup button below. After that just look
