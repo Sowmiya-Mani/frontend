883280216c1d341347f1d3a64cab352f9e9887fd
amarell <mujagicamar@gmail.com>
2022-06-01 17:54:16 +0200

    Go to profile from navigation bar

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,18 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Navbar, Container, Nav } from "react-bootstrap";
-
+import {
+  Navbar,
+  Container,
+  Nav,
+  Dropdown,
+  DropdownButton,
+} from "react-bootstrap";
+import jwt_decode from "jwt-decode";
+import usersService from "../../services/users";
 import styles from "./NavigationBar.module.scss";
 
 function NavigationBar() {
   const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem("token"));
+  const [userData, setUserData] = useState({});
   const navigate = useNavigate();
 
   const logout = () => {
     setIsLoggedIn(false);
+    setUserData({});
     localStorage.removeItem("token");
     window.location.reload();
   };
+
+  const goToProfile = () => {
+    navigate("/users/" + userData._id);
+  };
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      const userData = jwt_decode(localStorage.getItem("token"));
+      usersService
+        .getUserById({ id: userData.uid })
+        .then((res) => setUserData(res.data.data))
+        .catch((err) => console.log(err));
+    }
+  }, [isLoggedIn]);
 
   return (
     <div>
@@ -23,7 +46,18 @@ function NavigationBar() {
           </Navbar.Brand>
           <Nav>
             {isLoggedIn ? (
-              <Nav.Link onClick={logout}>Log out</Nav.Link>
+              <DropdownButton
+                id="dropdown-basic-button"
+                title={`${userData.first_name} ${userData.last_name}`}
+              >
+                <Dropdown.Item href="#/action-1" onClick={logout}>
+                  Logout
+                </Dropdown.Item>
+                <Dropdown.Item href="#/action-2" onClick={goToProfile}>
+                  My profile
+                </Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+              </DropdownButton>
             ) : (
               <>
                 <Nav.Link onClick={() => navigate("/login")}>Login</Nav.Link>
